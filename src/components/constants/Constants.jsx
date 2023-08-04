53fa943916477a1e73397d26f7b402c98489832d
iamshiv007 <iamshiv20032003@gmail.com>
2023-08-04 20:21:00 +0530

    Coutry option

---
+++ src/components/constants/Constants.jsx
@@ -0,0 +1,54 @@
+import { AiFillHome, AiTwotoneFire } from "react-icons/ai";
+import { MdSportsBaseball, MdSportsEsports } from "react-icons/md";
+import { BiCameraMovie, BiSolidBookReader } from "react-icons/bi";
+import { RiJavascriptFill } from "react-icons/ri";
+
+export const sidebarData = [
+  { name: "Home", icon: <AiFillHome /> },
+  { name: "Trending", icon: <AiTwotoneFire /> },
+  { name: "Entertainment", icon: <AiTwotoneFire /> },
+  { name: "Podcasts", icon: <AiTwotoneFire /> },
+  { name: "Sports and Highlightes", icon: <MdSportsBaseball /> },
+  { name: "Movies and Webseries", icon: <BiCameraMovie /> },
+  { name: "Health and Fitness", icon: <AiTwotoneFire /> },
+  { name: "Programming and Coding", icon: <RiJavascriptFill /> },
+  { name: "Food and Blogging", icon: <AiTwotoneFire /> },
+  { name: "Science and Fact", icon: <AiTwotoneFire /> },
+  { name: "Gaming and Live", icon: <MdSportsEsports /> },
+  { name: "Technology and Space", icon: <MdSportsEsports /> },
+  { name: "Tech and Startups", icon: <MdSportsEsports /> },
+  { name: "AI and ChatGpt", icon: <MdSportsEsports /> },
+];
+
+export const countries = [
+  {
+    name: "America",
+    countryCode: "US",
+    url: "https://t3.ftcdn.net/jpg/02/70/24/98/240_F_270249859_mf1Kyad7MO3Gb1BGvBahbB9SNttnVZO7.jpg",
+  },
+  {
+    name: "India",
+    countryCode: "IN",
+    url: "https://t4.ftcdn.net/jpg/02/81/47/57/240_F_281475718_rlQONmoS2E3CJtv0zFv2HwZ1weGhxpff.jpg",
+  },
+  {
+    name: "Germany",
+    countryCode: "DE",
+    url: "https://t3.ftcdn.net/jpg/04/44/28/64/240_F_444286454_6FR1VrzfVE8AJCwd28ft9T4pxEwH22Ng.jpg",
+  },
+  {
+    name: "Japan",
+    countryCode: "JP",
+    url: "https://t3.ftcdn.net/jpg/01/79/73/80/240_F_179738020_0cdBcea7tUpPoFTCiiVfl6p9chD28tQz.jpg",
+  },
+  {
+    name: "Canada",
+    countryCode: "CA",
+    url: "https://t3.ftcdn.net/jpg/01/71/57/72/240_F_171577280_Gj1SV9BV1vrvowWTexaiJW7OBj7uNgCT.jpg",
+  },
+  {
+    name: "England",
+    countryCode: "GB",
+    url: "https://t3.ftcdn.net/jpg/06/01/92/56/240_F_601925600_OPd3C0QuEE283YX2Fj6v3QtFFnkdtETF.jpg",
+  },
+];
